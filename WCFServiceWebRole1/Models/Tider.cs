4503f5a89a60556666f609236d4d9016cf6e2435
Daniel <danielwinther@hotmail.dk>
2015-12-01 12:54:30 +0100

    seneste

--- WCFServiceWebRole1/Models/Tider.cs
+++ WCFServiceWebRole1/Models/Tider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
+using System.Web.WebSockets;
 
 namespace WCFServiceWebRole1.Models
 {
@@ -8,13 +9,59 @@ namespace WCFServiceWebRole1.Models
     [Table("Tider")]
     public class Tider
     {
+        private TimeSpan _fra;
+        private TimeSpan _til;
+
         [DataMember]
         public int Id { get; set; }
 
-        [DataMember]
-        public TimeSpan Fra { get; set; }
+        public void CheckFra(string fra)
+        {
+            TimeSpan f = TimeSpan.Parse(fra);
+
+            if (f == new TimeSpan(24, 00, 00))
+            {
+                throw new ArgumentException(" udfyld en af felterne ");
+            }
+            if (f == new TimeSpan(-1, 0, 0))
+            {
+                throw new ArgumentException("tallet må ikke være minus ");
+            }
+        }
+
+        public void Checktil(string til)
+        {
+            TimeSpan t = TimeSpan.Parse(til);
+            if (t == new TimeSpan(24, 00, 00))
+            {
+                throw new ArgumentException(" udfyld en af felterne ");
+            }
+            if (t == new TimeSpan(-1, 0, 0))
+            {
+                throw new ArgumentException("tallet må ikke være minus ");
+            }
+        }
 
         [DataMember]
-        public TimeSpan Til { get; set; }
+        public TimeSpan Fra
+        {
+            get { return _fra; }
+            set
+            {
+                CheckFra(value.ToString());
+                _fra = value;
+
+            }
+        }
+        [DataMember]
+        public TimeSpan Til
+        {
+            get { return _til; }
+            set
+            {
+                Checktil(value.ToString());
+                _til = value;
+            }
+        }
     }
 }
